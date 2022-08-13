2c21bcf72cd32e03780c677ca03477f3ae3b40b4
mpn2005 <mpn-2005@tut.by>
2022-08-14 00:25:10 +0300

    Убираем ошибки, которые возникают при колизиях, в файловом кэше

--- upload/system/library/cache/file.php
+++ upload/system/library/cache/file.php
@@ -14,7 +14,7 @@ class File {
 
 				if ($time < time()) {
 					if (file_exists($file)) {
-						unlink($file);
+						@unlink($file);
 					}
 				}
 			}
@@ -25,17 +25,19 @@ class File {
 		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', $key) . '.*');
 
 		if ($files) {
-			$handle = fopen($files[0], 'r');
+			$handle = @fopen($files[0], 'r');
 
-			flock($handle, LOCK_SH);
+			if ($handle) {
+				@flock($handle, LOCK_SH);
 
-			$data = fread($handle, filesize($files[0]));
+				$data = @fread($handle, @filesize($files[0]));
 
-			flock($handle, LOCK_UN);
+				@flock($handle, LOCK_UN);
 
-			fclose($handle);
+				@fclose($handle);
 
-			return json_decode($data, true);
+				return json_decode($data, true);
+			}
 		}
 
 		return false;
@@ -48,15 +50,17 @@ class File {
 
 		$handle = fopen($file, 'w');
 
-		flock($handle, LOCK_EX);
+		if ($handle) {
+			@flock($handle, LOCK_EX);
 
-		fwrite($handle, json_encode($value));
+			@fwrite($handle, json_encode($value));
 
-		fflush($handle);
+			@fflush($handle);
 
-		flock($handle, LOCK_UN);
+			@flock($handle, LOCK_UN);
 
-		fclose($handle);
+			@fclose($handle);
+		}
 	}
 
 	public function delete($key) {
@@ -69,7 +73,7 @@ class File {
 		if ($files) {
 			foreach ($files as $file) {
 				if (file_exists($file)) {
-					unlink($file);
+					@unlink($file);
 				}
 			}
 		}
