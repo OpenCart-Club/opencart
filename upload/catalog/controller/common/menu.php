59ba14ba68ba8e6242081a2406304c65033bffc8
mpn2005 <mpn-2005@tut.by>
2023-03-30 04:11:48 +0400

    Обновление демо данных
    - Небольшие правки стилей
    - Убраны ненужные изображения
    - Небольшие исправления в шаблонах темы default

--- upload/catalog/controller/common/menu.php
+++ upload/catalog/controller/common/menu.php
@@ -10,35 +10,33 @@ class ControllerCommonMenu extends Controller {
 
 		$data['categories'] = array();
 
-		$categories = $this->model_catalog_category->getCategories(0);
+		$categories = $this->model_catalog_category->getTopCategories();
 
 		foreach ($categories as $category) {
-			if ($category['top']) {
-				// Level 2
-				$children_data = array();
+			// Level 2
+			$children_data = array();
 
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
+			$children = $this->model_catalog_category->getCategories($category['category_id']);
 
-				foreach ($children as $child) {
-					$filter_data = array(
-						'filter_category_id'  => $child['category_id'],
-						'filter_sub_category' => true
-					);
+			foreach ($children as $child) {
+				$filter_data = array(
+					'filter_category_id'  => $child['category_id'],
+					'filter_sub_category' => true
+				);
 
-					$children_data[] = array(
-						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
-					);
-				}
-
-				// Level 1
-				$data['categories'][] = array(
-					'name'     => $category['name'],
-					'children' => $children_data,
-					'column'   => $category['column'] ? $category['column'] : 1,
-					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+				$children_data[] = array(
+					'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+					'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
 				);
 			}
+
+			// Level 1
+			$data['categories'][] = array(
+				'name'     => $category['name'],
+				'children' => $children_data,
+				'column'   => $category['column'] ? $category['column'] : 1,
+				'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+			);
 		}
 
 		return $this->load->view('common/menu', $data);
--- upload/catalog/model/catalog/category.php
+++ upload/catalog/model/catalog/category.php
@@ -12,6 +12,12 @@ class ModelCatalogCategory extends Model {
 		return $query->rows;
 	}
 
+	public function getTopCategories() {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category c LEFT JOIN " . DB_PREFIX . "category_description cd ON (c.category_id = cd.category_id) LEFT JOIN " . DB_PREFIX . "category_to_store c2s ON (c.category_id = c2s.category_id) WHERE c.top = '1' AND cd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND c2s.store_id = '" . (int)$this->config->get('config_store_id') . "'  AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)");
+
+		return $query->rows;
+	}
+
 	public function getCategoryFilters($category_id) {
 		$implode = array();
 
