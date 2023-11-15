9dd3d7d1c53f11b610a6b5084e8460b293621b1e
mpn2005 <mpn-2005@tut.by>
2023-11-15 15:30:13 +0400

    Доработка библиотеки session/db
    Исправлено сохранение сессии при переключении валюты или языка.

--- upload/system/library/session/db.php
+++ upload/system/library/session/db.php
@@ -2,12 +2,19 @@
 namespace Session;
 
 final class DB {
+	private $db;
+	private $config;
+	
+	private $currency;
+	private $language;
 	public $maxlifetime;
 
 	public function __construct($registry) {
 		$this->db = $registry->get('db');
-		
 		$this->config = $registry->get('config');
+		
+		$this->language = $this->config->get('config_language');
+		$this->currency = $this->config->get('config_currency');
 
 		$this->maxlifetime = ini_get('session.gc_maxlifetime') !== null ? (int)ini_get('session.gc_maxlifetime') : 1440;
 
@@ -18,7 +25,12 @@ final class DB {
 		$query = $this->db->query("SELECT `data` FROM `" . DB_PREFIX . "session` WHERE `session_id` = '" . $this->db->escape($session_id) . "' AND `expire` > '" . $this->db->escape(date('Y-m-d H:i:s', time())) . "'");
 
 		if ($query->num_rows) {
-			return json_decode($query->row['data'], true);
+			$data = json_decode($query->row['data'], true);
+			
+			if (!empty($data['language'])) $this->language = $data['language'];
+			if (!empty($data['currency'])) $this->currency = $data['currency'];
+			
+			return $data;
 		} else {
 			return array();
 		}
@@ -26,27 +38,27 @@ final class DB {
 
 	public function write($session_id, $data) {
 		if ($session_id) {
-            if (empty($data)) {
-                return true;
-            }
-            
-            $session_empty = true;
-            
-            foreach ($data as $key => $value) {
-                if ($key == 'language' && $value == $this->config->get('config_language')) {
-                    continue;
-                }
-                if ($key == 'currency' && $value == $this->config->get('config_currency')) {
-                    continue;
-                }
-                $session_empty = false;
-                break;
-            }
-            
-            if ($session_empty) {
-                return true;
-            }
-            
+			if (empty($data)) {
+				return true;
+			}
+			
+			$session_empty = true;
+			
+			foreach ($data as $key => $value) {
+				if ($key == 'language' && $value == $this->language) {
+					continue;
+				}
+				if ($key == 'currency' && $value == $this->currency) {
+					continue;
+				}
+				$session_empty = false;
+				break;
+			}
+			
+			if ($session_empty) {
+				return true;
+			}
+			
 			$this->db->query("REPLACE INTO `" . DB_PREFIX . "session` SET `session_id` = '" . $this->db->escape($session_id) . "', `data` = '" . $this->db->escape(json_encode($data)) . "', `expire` = '" . $this->db->escape(date('Y-m-d H:i:s', time() + (int)$this->maxlifetime)) . "'");
 		}
 
