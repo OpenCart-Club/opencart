7aecb6158bb2669366995b57b3455c5f569ac95c
mpn2005 <mpn-2005@tut.by>
2023-12-14 04:47:47 +0400

    Ускорение и оптимизация работы карты сайта Google Sitemap:
    - Добавлена отдельная модель для карты сайта
    - В запросах выбираются только необходимые данные
    - Для экономии памяти и ускорения работы используется поддержка queryFetchable

--- upload/catalog/controller/extension/feed/google_sitemap.php
+++ upload/catalog/controller/extension/feed/google_sitemap.php
@@ -5,12 +5,10 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 			$output  = '<?xml version="1.0" encoding="UTF-8"?>';
 			$output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
 
-			$this->load->model('catalog/product');
+			$this->load->model('extension/feed/google_sitemap');
 			$this->load->model('tool/image');
 
-			$products = $this->model_catalog_product->getProducts();
-
-			foreach ($products as $product) {
+			foreach ($this->model_extension_feed_google_sitemap->getProducts() as $product) {
 				$output .= '<url>';
 				$output .= '  <loc>' . $this->url->link('product/product', 'product_id=' . $product['product_id']) . '</loc>';
 				$output .= '  <changefreq>weekly</changefreq>';
@@ -28,15 +26,15 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 				$output .= '</url>';
 			}
 
-			$this->load->model('catalog/category');
+			foreach ($this->model_extension_feed_google_sitemap->getCategories() as $category) {
+				$output .= '<url>';
+				$output .= '  <loc>' . $this->url->link('product/category', 'path=' . $category['category_id']) . '</loc>';
+				$output .= '  <changefreq>weekly</changefreq>';
+				$output .= '  <priority>0.7</priority>';
+				$output .= '</url>';
+			}
 
-			$output .= $this->getCategories(0);
-
-			$this->load->model('catalog/manufacturer');
-
-			$manufacturers = $this->model_catalog_manufacturer->getManufacturers();
-
-			foreach ($manufacturers as $manufacturer) {
+			foreach ($this->model_extension_feed_google_sitemap->getManufacturers() as $manufacturer) {
 				$output .= '<url>';
 				$output .= '  <loc>' . $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $manufacturer['manufacturer_id']) . '</loc>';
 				$output .= '  <changefreq>weekly</changefreq>';
@@ -44,11 +42,7 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 				$output .= '</url>';
 			}
 
-			$this->load->model('catalog/information');
-
-			$informations = $this->model_catalog_information->getInformations();
-
-			foreach ($informations as $information) {
+			foreach ($this->model_extension_feed_google_sitemap->getInformations() as $information) {
 				$output .= '<url>';
 				$output .= '  <loc>' . $this->url->link('information/information', 'information_id=' . $information['information_id']) . '</loc>';
 				$output .= '  <changefreq>weekly</changefreq>';
@@ -61,23 +55,5 @@ class ControllerExtensionFeedGoogleSitemap extends Controller {
 			$this->response->addHeader('Content-Type: application/xml');
 			$this->response->setOutput($output);
 		}
-	}
-
-	protected function getCategories($parent_id) {
-		$output = '';
-
-		$results = $this->model_catalog_category->getCategories($parent_id);
-
-		foreach ($results as $result) {
-			$output .= '<url>';
-			$output .= '  <loc>' . $this->url->link('product/category', 'path=' . $result['category_id']) . '</loc>';
-			$output .= '  <changefreq>weekly</changefreq>';
-			$output .= '  <priority>0.7</priority>';
-			$output .= '</url>';
-
-			$output .= $this->getCategories($result['category_id']);
-		}
-
-		return $output;
 	}
 }
