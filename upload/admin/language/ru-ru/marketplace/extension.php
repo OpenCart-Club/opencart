8964fa6e3997a81f295532b8e49f0e3a9d058020
awaro <webadmin@awaro.ru>
2024-05-28 14:39:51 +0300

    Update extension.php

--- upload/admin/language/ru-ru/marketplace/extension.php
+++ upload/admin/language/ru-ru/marketplace/extension.php
@@ -7,6 +7,3 @@ $_['text_success']  = 'Данные успешно сохранены!';
 $_['text_list']     = 'Список дополнений';
 $_['text_type']     = 'Выберите тип дополнения';
 $_['text_filter']   = 'Фильтр';
-
-// Help
-$_['help_doc']      = 'https://opencart.club/doc/doc-extension-module-install/';
