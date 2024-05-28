78b23f96f340ec812679299e57069ae31c556493
awaro <webadmin@awaro.ru>
2024-05-28 18:20:02 +0300

    Update event.php
    add doc

--- upload/admin/language/ru-ru/marketplace/event.php
+++ upload/admin/language/ru-ru/marketplace/event.php
@@ -16,5 +16,8 @@ $_['column_status']     = 'Статус';
 $_['column_sort_order'] = 'Порядок сортировки';
 $_['column_action']     = 'Действие';
 
+//Help
+$_['help_doc']          = 'https://opencart.club/doc/doc-extension-event/';
+
 // Error
 $_['error_permission']  = 'Недостаточно прав для внесения изменений!';
